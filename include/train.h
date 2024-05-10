297e971a80cca6bb4536bfe2d1453b6ce833631d
OlliK23 <160269347+OlliK23@users.noreply.github.com>
2024-05-10 21:10:24 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -3,18 +3,23 @@
 #define INCLUDE_TRAIN_H_
 
 class Train {
- private:
-  struct Cage {
-    bool light; // состояние лампочки
-    Cage *next;
-    Cage *prev;
-  };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Cage *first; // точка входа в поезд (первый вагон)
- public:
-  Train();
-  void addCage(bool light); // добавить вагон с начальным состоянием лампочки
-  int getLength();          // вычислить длину поезда
-  int getOpCount();         // вернуть число переходов (из вагона в вагон)
+private:
+    struct Cage {
+        bool light;
+        Cage *next;
+        Cage *prev;
+        Cage(bool lightStatus) : light(lightStatus), next(nullptr), prev(nullptr) {}
+    };
+
+    Cage *first;
+    int countOp;
+
+public:
+    Train();
+    ~Train();
+    void addCage(bool light);
+    int getLength();
+    int getOpCount();
 };
+
 #endif  // INCLUDE_TRAIN_H_
